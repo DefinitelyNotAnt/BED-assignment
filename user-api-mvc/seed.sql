b966d2a7e70f8b2c7567972212368d508414043f
supnie <150310259+SupNie@users.noreply.github.com>
2024-07-26 21:56:04 +0800

    Update my sql

---
+++ user-api-mvc/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO Places (name, description, operatinghours, address)
+VALUES
+('Old Folks Club', 'It offers events for the elderly to bond together', '9am - 9pm', 'Woodlands Avenue 4'),
+('Elderly Support Club', 'It offers support for the elderly','7am - 7pm', 'Yishun Avenue 4');
